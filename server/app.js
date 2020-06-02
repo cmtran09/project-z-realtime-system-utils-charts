be8b1f3b6d239cd48b4716ad61f05d9f90cb44c5
cmtran09 <cmtran@hotmail.co.uk>
2020-06-02 17:28:39 +0100

    systeminformation added for more system metrics

--- server/app.js
+++ server/app.js
@@ -17,7 +17,21 @@ const cpu = osu.cpu
 const drive = osu.drive
 
 
+const si = require('systeminformation');
 
+si.cpu()
+  .then(data => {
+    console.log('CPU Information:');
+    console.log('- manufucturer: ' + data.manufacturer);
+    console.log('- brand: ' + data.brand);
+    console.log('- speed: ' + data.speed);
+    console.log('- cores: ' + data.cores);
+    console.log('- physical cores: ' + data.physicalCores);
+    console.log('...');
+  })
+  .catch(error => console.error(error));
+
+si.cpuTemperature().then(data => console.log('tempppp', data))
 // // for deployment
 
 // const path = require('path')
