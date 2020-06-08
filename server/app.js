77c9d2ed84d4b0f4bcaa8a17aeeb51db26fe3f41
cmtran09 <cmtran@hotmail.co.uk>
2020-06-08 22:28:30 +0100

    server app.js refactored

--- server/app.js
+++ server/app.js
@@ -21,14 +21,10 @@ const drive = osu.drive
 
 const si = require('systeminformation');
 
+// for deployment
 
-si.cpuTemperature().then(data => console.log('tempppp', data))
-
-si.graphics().then(data => console.log("GRAPHICS", data));
-// // for deployment
-
-// const path = require('path')
-// const dist = path.join(__dirname, '../dist')
+const path = require('path')
+const dist = path.join(__dirname, '../dist')
 
 const router = require('./config/router')
 
@@ -36,63 +32,6 @@ app.use((req, resp, next) => {
   console.log(`${req.method} to ${req.url}`)
   next()
 })
-
-osUtils.cpuUsage((value) => console.log(`CPU Usage (%): ${value}`))
-
-osUtils.cpuFree((value) => console.log(`cpu free: ${value}`))
-console.log(`platform: ${osUtils.platform()}`)
-console.log(`freemem: ${osUtils.freemem()}`)
-console.log(`totalmem: ${osUtils.totalmem()}`)
-console.log(`freememPercentage: ${osUtils.freememPercentage()}`)
-console.log(`sysUptime: ${osUtils.sysUptime()}`)
-console.log(`processUptime: ${osUtils.processUptime()}`)
-console.log(`cpu count: ${osUtils.cpuCount()}`)
-
-console.log(`average load for 1 minutes: ${osUtils.loadavg(1)}`)
-console.log(`average load for 5 minutes: ${osUtils.loadavg(5)}`)
-console.log(`average load for 15 minutes: ${osUtils.loadavg(15)}`)
-
-console.log(`cpu model`, cpu.model())
-
-osu.drive.info()
-  .then(info => {
-    console.log('hdddrive:', info)
-  })
-  .catch(err => console.log(err))
-
-osu.drive.free()
-  .then(info => {
-    console.log('hdddrive free:', info)
-  })
-  .catch(err => console.log(err))
-
-// var netstat = osu.netstat
-
-osu.netstat.inOut()
-  .then(info => {
-    console.log(info)
-  })
-  .catch(err => console.log(err))
-
-osu.mem.info()
-  .then(info => console.log('mem', info))
-  .catch(err => console.log(err))
-
-
-osu.openfiles.openFd()
-  .then(info => console.log('mem', info))
-  .catch(err => console.log('openfiles', err))
-
-osu.os.oos()
-  .then(info => console.log('platform', info))
-  .catch(err => console.log('osu.os.oos()', err))
-
-console.log('playt', osu.os.platform())
-
-console.log('IP', osu.os.ip())
-console.log('Hostname', osu.os.hostname())
-console.log('OS TYPE', osu.os.type())
-console.log('OS ARCH', osu.os.arch())
 
 io.on('connection', client => {
   //convert data to percent and 2 decimal places
@@ -188,14 +127,14 @@ io.on('connection', client => {
 
 
 // for development
-app.use('/', router)
+// app.use('/', router)
 
 // // for deployment
-// app.use('/', express.static(dist))
+app.use('/', express.static(dist))
 
 // // for deployment
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(dist, 'index.html'))
-// });
+app.get('*', function(req, res) {
+  res.sendFile(path.join(dist, 'index.html'))
+});
 
 server.listen(PORT, () => console.log(`Server up and running on port ${PORT}`))
