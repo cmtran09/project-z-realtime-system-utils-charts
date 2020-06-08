576d21dc5bd23237c83938146c629f2ff8059b81
cmtran09 <cmtran@hotmail.co.uk>
2020-06-08 20:06:11 +0100

    responsive battery chart media query added

--- client/components/Battery/Battery.js
+++ client/components/Battery/Battery.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 
 import BatteryChart from './BatteryChart/BatteryChart'
+import './Battery.scss'
 
 const socket = io('http://localhost:5000', {
   transports: ['webscoket', 'polling']
@@ -20,11 +21,8 @@ export default function Battery() {
   return (
     //render only if it has a battery (hasbattery) if not cancel
     <div 
-    style={{ width: '100%', height: '200px' }}
+    style={{ width: '100%', height: '150px' }}
     >
-      <p>Manufacturer: {batteryData.manufacturer}</p>
-      <p>Serial no: {batteryData.serial}</p>
-      <p>{`Cycle Count: ${batteryData.cyclecount}`}</p>
       <BatteryChart
         chartData={[{
           "name": batteryData ? `Battery Capacity (${batteryData.capacityUnit})` : '',
@@ -33,7 +31,10 @@ export default function Battery() {
           "Current": batteryData ? batteryData.currentcapacity : 0
         }]}
         capacityUnit={batteryData.capacityUnit}
-      />
+        />
+        <p className='battery-info'>Manufacturer: {batteryData.manufacturer}</p>
+        <p className='battery-info'>Serial no: {batteryData.serial}</p>
+        <p className='battery-info'>{`Cycle Count: ${batteryData.cyclecount}`}</p>
     </div>
   )
 }
